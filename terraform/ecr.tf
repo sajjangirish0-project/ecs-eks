39020c242027c558f32453423b2d48b952a3feba
Girish <girish@Mac.lan>
2025-12-17 23:02:41 +0530

    deploy the eks and ecs

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,9 +2,16 @@ module "ecr" {
   source  = "terraform-aws-modules/ecr/aws"
   version = "~> 1.6"
 
+  # Critical: Don't create repository since it already exists
+  create_repository = false
+  
+  # Name of the EXISTING repository
   repository_name = "${var.project_name}-repository"
   repository_type = "private"
 
+  # Only manage the lifecycle policy on the existing repository
+  create_lifecycle_policy = true
+  
   repository_lifecycle_policy = jsonencode({
     rules = [
       {
